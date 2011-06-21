80bfce4e8ab9334fa1871154c2a650b1dd29ddf5
Peter Robinett <peter@bubblefoundry.com>
2011-06-21 15:17:23 +0200

    Add UUID function

--- mathplus.js
+++ mathplus.js
@@ -317,5 +317,13 @@ var M = {
   // based on an equation on Wikipedia: http://en.wikipedia.org/wiki/Closure_(computer_science)
   'derivative': function derivative(f, x) {
     return (f(x + M.SMALL_NUMBER) - f(x)) / M.SMALL_NUMBER;
+  },
+  // RFC 4122 version 4 UUID
+  // from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
+  'UUID': function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
   }
 };
